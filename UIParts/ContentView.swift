81baddcd5323e174a7471cb9ea8f4c00b9da028f
Ryo Hamma <hyanma@live.jp>
2021-08-24 04:16:28 +0900

    Initial Commit

---
+++ UIParts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIParts
+//
+//  Created by Ryo Hanma on 2021/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIParts/UIPartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPartsApp.swift
+//  UIParts
+//
+//  Created by Ryo Hanma on 2021/08/24.
+//
+
+import SwiftUI
+
+@main
+struct UIPartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
